1171f73327ff7f177ca81ef6691b91f1383f20da
Uday Hiwarale <uday.hiwarale@avizva.com>
2018-06-13 18:31:34 +0530

    debounceTime functionality added

--- src/js/components/container/App.component.js
+++ src/js/components/container/App.component.js
@@ -5,10 +5,17 @@ import { Header } from 'views/Header.view';
 import { Footer } from 'views/Footer.view';
 import { MessageBus } from 'modules/MessageBus';
 
-import { pluck } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { pluck, debounceTime } from 'rxjs/operators';
 import { getAllUsers } from 'services/user.service';
 
 export class App extends React.Component {
+    constructor() {
+        super();
+
+        this.inputRef = React.createRef();
+    }
+
     makeRequest() {
 
         // getAllUsers( hander, transformer )
@@ -42,6 +49,12 @@ export class App extends React.Component {
     }
 
     componentDidMount() {
+
+        // RxJS from event
+        fromEvent( this.inputRef.current, 'input' )
+        .pipe( debounceTime( 1000 ) )
+        .subscribe( this.makeRequest.bind( this ) );
+
         
         // emit message bus `CHAT_MESSAGE` event
         MessageBus.emit( 'CHAT_MESSAGE', {
@@ -58,6 +71,11 @@ export class App extends React.Component {
 
                 <button onClick={ this.makeRequest.bind( this ) }>Get user list</button>
                 <button onClick={ this.cancelRequest.bind( this ) }>Cancel request</button>
+
+
+                <br/>
+
+                <input type="text" ref={ this.inputRef } />
 
                 <RenderRoutes currentPage={ this.props.currentPage } />
 
