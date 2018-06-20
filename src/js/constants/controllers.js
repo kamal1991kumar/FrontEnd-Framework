b5babf4064882e5ccda3b9ea8444a808b2877a22
Uday Hiwarale <uday.hiwarale@avizva.com>
2018-06-20 16:45:41 +0530

    handlerbars template addition

--- src/js/constants/controllers.js
+++
@@ -1,1 +0,0 @@
-export const INDEX_CONTROLLER = 'index_controller';
--- src/js/views/Footer.view.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-    return (
-        <div className='site-footer'>
-            Welcome to AVIZVA Solutions Pvt. Ltd. All rights reserved 2011-2018.
-        </div>
-    );
-};
